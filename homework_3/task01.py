2af48c2d9d498f083ba68cfbe0eedbf72d99b2ba
Shandirov Kantemir <kan.shandir94@gmail.com>
2021-04-03 18:08:54 +0300

    added homework_3

---
+++ homework_3/task01.py
@@ -0,0 +1,24 @@
+"""
+Armstrong number is a number that is the sum of its own digits each raised to the power of the number of digits.
+https://en.wikipedia.org/wiki/Narcissistic_number
+Examples:
+- 9 is an Armstrong number, 9 = 9^1 = 9
+- 10 is not: 10 != 1^2 + 0^2 = 1
+- 153 is : 153 = 1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153
+Write a function that detects if a number is Armstrong number in functionaly style:
+ - use map or other utilities from functools library,
+ - use anonymous functions (or use function as argument)
+ - do not use loops, preferably using list comprehensions
+### Example function signature and call
+"""
+
+
+def is_armstrong(number: int) -> bool:
+    n_string = str(number)
+    n_length = len(n_string)
+    first_step = map(int, n_string)
+    second_step = map(lambda x: x ** n_length, first_step)
+    if sum(second_step) == number:
+        return True
+    else:
+        return False
